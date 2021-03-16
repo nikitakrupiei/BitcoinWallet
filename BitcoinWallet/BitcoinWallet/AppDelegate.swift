238ce928463d0355c898750ad81ce92c0f9d3581
nikitakrupiei <51328883+nikitakrupiei@users.noreply.github.com>
2021-03-16 08:39:30 +0200

    initial commit

---
+++ BitcoinWallet/BitcoinWallet/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  BitcoinWallet
+//
+//  Created by admin on 16.03.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
